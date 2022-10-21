c1aea4e16b3e862eabdde3914fdd12f4f1563afa
misbah099 <111556255+misbah099@users.noreply.github.com>
2022-10-21 10:33:59 +0530

    Update BankMain.java

--- 20.10.2022/BankMain.java
+++ 20.10.2022/BankMain.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 public class BankMain {
 
 	public static void main(String[] args) {
-		BankDetail b1=new BankDetail();
-		BankDetail b2=new BankDetail();
-		b1.createAccount(1111, "Misbah", 100000);
-        b1.addAmount(3000);
-	b1.withDrawAmount(20000);
-        b1.getAmount();
-        b2.createAccount(2222, "Sourav", 44000);
-        b2.addAmount(2000);
-        b2.withDrawAmount(10000);
-        b2.getAmount();
+		BankDetail b1=new BankDetail();//creating the object of 1st account
+		BankDetail b2=new BankDetail();//creating the object of second account
+		b1.createAccount(1111, "Misbah", 100000);//details of 1st account
+        b1.addAmount(3000);//amount added by 1st account
+	b1.withDrawAmount(20000);//amount withdraw by 1st account
+        b1.getAmount();//print the updated details
+        b2.createAccount(2222, "Sourav", 44000);//creating the 2nd account
+        b2.addAmount(2000);//adding amount by 2nd
+        b2.withDrawAmount(10000);//withdrawing the balance from 2nd
+        b2.getAmount();//updated detail of 2nd acc
 	}
 	  }
 

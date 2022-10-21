76908efc27317a672b5ba5a5140ac1b0d9127bf1
misbah099 <111556255+misbah099@users.noreply.github.com>
2022-10-21 10:08:37 +0530

    Update BankMain.java

--- 20.10.2022/BankMain.java
+++ 20.10.2022/BankMain.java
@@ -9,10 +9,12 @@ public class BankMain {
 		BankDetail b2=new BankDetail();
 		b1.createAccount(1111, "Misbah", 100000);
         b1.addAmount(3000);
+	b1.withDrawAmount(20000);
         b1.getAmount();
-        b1.createAccount(2222, "Sourav", 44000);
-        b1.addAmount(2000);
-        b1.getAmount();
+        b2.createAccount(2222, "Sourav", 44000);
+        b2.addAmount(2000);
+        b2.withDrawAmount(10000);
+        b2.getAmount();
 	}
 	  }
 

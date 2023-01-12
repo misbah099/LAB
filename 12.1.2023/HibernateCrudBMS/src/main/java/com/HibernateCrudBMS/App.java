080258c856e1bc0bf16ce072a20d8f4ef4c4e598
misbah099 <111556255+misbah099@users.noreply.github.com>
2023-01-12 13:51:43 +0530

    Add files via upload

---
+++ 12.1.2023/HibernateCrudBMS/src/main/java/com/HibernateCrudBMS/App.java
@@ -0,0 +1,38 @@
+package com.HibernateCrudBMS;
+
+import java.util.Scanner;
+
+import com.HibernateCrudBMS.daoimpl.BankDaoImpl;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	BankDaoImpl dao=new BankDaoImpl();
+        char c;
+        do {
+        	Scanner sc=new Scanner(System.in);
+        	System.out.println("Press 1 for add Account \n Press 2 for display Account \n"
+        			+"Press 3 for update \n Press 4 for delete \n Press 5 for exit");
+        	int a=sc.nextInt();
+        	switch(a) {
+        	case 1:dao.addAccount();
+        	break;
+        	case 2:dao.fetchAccount();
+        	break;
+        	case 3:dao.updateAccount();
+        	break;
+        	case 4:dao.deleteAccount();
+        	break;
+        	case 5:System.exit(0);
+        	break;
+        	default:System.out.println("Invalid choice!");
+        	}
+        	System.out.println("Do you want to continue? Y/N");
+        	c=sc.next().charAt(0);
+        }while(c=='y'||c=='Y');
+        System.out.println("Thank you");
+        }
+    }
+

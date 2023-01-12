8295241afcec33f574df5d6eecf78c387e304c0c
misbah099 <111556255+misbah099@users.noreply.github.com>
2023-01-12 15:34:13 +0530

    Add files via upload

---
+++ 12.1.2023/HibernateCrudBMS/src/main/java/com/HibernateCrudBMS/dao/BankDao.java
@@ -0,0 +1,8 @@
+package com.HibernateCrudBMS.dao;
+
+public interface BankDao {
+	public void addAccount();
+	public void updateAccount();
+	public void deleteAccont();
+	public void fetchAccount();
+}
---
+++ 12.1.2023/HibernateCrudBMS/src/main/java/com/HibernateCrudBMS/entity/Bank.java
@@ -0,0 +1,35 @@
+package com.HibernateCrudBMS.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="bank_Information")
+public class Bank {
+@Id
+@GeneratedValue(strategy=GenerationType.AUTO)
+private int accountNo;
+@Column(length=20,nullable=false)
+private String name;
+@Column(length=20,nullable=false,unique=true)
+private String ifsccode;
+@Column(length=11,nullable=false, unique=true)
+private long phone;
+@Column(length=11,nullable=false)
+private int deposit;
+@Column(length=25,nullable=false,unique=true)
+private String email;
+}

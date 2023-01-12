3d5fc5488472b2b43da9cd1ef87d12fe7e24f869
misbah099 <111556255+misbah099@users.noreply.github.com>
2023-01-12 15:28:19 +0530

    Delete 12.1.2023/HibernateCrudBMS directory

--- 12.1.2023/HibernateCrudBMS/src/main/java/com/HibernateCrudBMS/dao/BankDao.java
+++
@@ -1,8 +0,0 @@
-package com.HibernateCrudBMS.dao;
-
-public interface BankDao {
-	public void addAccount();
-	public void updateAccount();
-	public void deleteAccont();
-	public void fetchAccount();
-}
